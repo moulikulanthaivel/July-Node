29a820aaa59ad933219d06d174eb7e218b55fee5
mouli.k <134676928+moulikulanthaivel@users.noreply.github.com>
2024-07-11 09:35:08 +0530

    okie

--- ExpressJS/staticProj/App.js
+++ ExpressJS/staticProj/App.js
@@ -6,6 +6,8 @@ const App = express()
 
 let index = path.join(process.cwd(),"data","index.html");
 let about = path.join(process.cwd(),"data","about.html");
+let services = path.join(process.cwd(),"data","services.html");
+let contact = path.join(process.cwd(),"data","contact.html");
 
 dotenv.config({path:"./setting/config.env"})
 let port = process.env.PORT
@@ -23,7 +25,19 @@ App.get("/about",(req,res)=>{
     })
 })
 
+App.get("/services",(req,res)=>{
+    res.sendFile(services,(err)=>{
+        if(err) throw err
+    })
+})
+
+App.get("/contact",(req,res)=>{
+    res.sendFile(contact,(err)=>{
+        if(err) throw err
+    })
+})
+
 App.listen(port,host,(err)=>{
     if(err) throw err
-    console.log(`http://${host}:${port}`)
+    console.log(`Server running successfully : http://${host}:${port}/`)
 })
