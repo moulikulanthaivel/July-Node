73e11f6a862a9c19b91f75e72a1447c5455e66cf
mouli.k <134676928+moulikulanthaivel@users.noreply.github.com>
2024-07-15 21:57:09 +0530

    ok

--- CRUD-JSON/router/empRouter.js
+++ CRUD-JSON/router/empRouter.js
@@ -7,15 +7,21 @@ empRouter.get("/read",async (req,res)=>{
     let employees = await getEmployees()
     res.status(200).json(employees)
 })
+
+
 empRouter.post("/create",(req,res)=>{})
+
+
 empRouter.put("/update/:id",(req,res)=>{})
+
+
 empRouter.delete("/delete/:id",(req,res)=>{})
 
 let getEmployees =()=>{
-    let employees = fs.readFileSync("data.json" , 'utf-8' )
+    let employees = fs.readFileSync("data.json",'utf-8')
     return JSON.parse(employees)
 
 }
-let saveEmployees =()=>{}
+let saveEmployee = ()=>{}
 
 export default empRouter
